6d82846eb748b62465a19a56e8a51361a0a48c22
viniciusminas <viniciusminas_@hotmail.com>
2025-05-28 20:39:43 -0300

    add select dropdown with bootstrap

--- resources/views/reserva/create.blade.php
+++ resources/views/reserva/create.blade.php
@@ -2,20 +2,29 @@
 
 @section('content')
 <div class="container mt-4">
-    <h2 class="mb-4">Reserva</h2>
+    <h2 class="mb-4">Reserva de Livro</h2>
 
     <form action="{{ route('reserva.create') }}" method="POST">
         @csrf
-        @method('POST')
 
         <div class="mb-3">
             <label for="livro" class="form-label">Livro:</label>
-            <input type="text" class="form-control" name="livro" id="livro" required>
+            <select class="form-select" name="livro" id="livro" required>
+                <option value="">Selecione um livro</option>
+                @foreach($livros as $livro)
+                    <option value="{{ $livro['id'] }}">{{ $livro['titulo'] }}</option>
+                @endforeach
+            </select>
         </div>
 
         <div class="mb-3">
             <label for="pessoa" class="form-label">Pessoa:</label>
-            <input type="text" class="form-control" name="pessoa" id="pessoa" required>
+            <select class="form-select" name="pessoa" id="pessoa" required>
+                <option value="">Selecione uma pessoa</option>
+                @foreach($pessoas as $pessoa)
+                    <option value="{{ $pessoa['id'] }}">{{ $pessoa['nome'] }}</option>
+                @endforeach
+            </select>
         </div>
 
         <button type="submit" class="btn btn-success">Reservar</button>
