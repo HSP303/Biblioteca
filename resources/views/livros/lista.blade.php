af93c2a2b4f7eb941a8574c139693b13ee49d123
viniciusminas <viniciusminas_@hotmail.com>
2025-05-31 20:06:15 -0300

    Update lista.blade.php

--- resources/views/livros/lista.blade.php
+++ resources/views/livros/lista.blade.php
@@ -3,6 +3,28 @@
 @section('content')
     <h1 class="mb-4">Livros Cadastrados</h1>
 
+    <form method="GET" action="{{ route('livro.get') }}" class="mb-4">
+    <div class="row">
+        <div class="col">
+            <input type="text" name="titulo" class="form-control" placeholder="Título"
+                value="{{ $filtros['titulo'] ?? '' }}">
+        </div>
+        <div class="col">
+            <input type="text" name="autor" class="form-control" placeholder="Autor"
+                value="{{ $filtros['autor'] ?? '' }}">
+        </div>
+        <div class="col">
+            <input type="number" name="ano" class="form-control" placeholder="Ano"
+                value="{{ $filtros['ano'] ?? '' }}">
+        </div>
+        <div class="col">
+            <button type="submit" class="btn btn-primary">Buscar</button>
+            <a href="{{ route('livro.get') }}" class="btn btn-secondary">Limpar</a>
+        </div>
+    </div>
+    </form>
+
+
     <table class="table table-striped table-bordered">
         <thead class="table-dark">
             <tr>
